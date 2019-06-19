afb9ab7b6159e02c62f01910c8f5ac17169328f3
Polly Reynolds <polly.c.reynolds@gmail.com>
2019-06-19 18:16:18 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,17 +1,17 @@
 def welcome
-puts "Welcome to the Blackjack Table"
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-card = rand(1..11)
+  card = rand(1..11) 
 end
 
 def display_card_total(card_total)
-puts "Your cards add up to #{card_total}"
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
-puts "Type 'h' to hit or 's' to stay"
+  puts "Type 'h' to hit or 's' to stay"
 end
 
 def get_user_input
@@ -19,15 +19,15 @@ def get_user_input
 end
 
 def end_game(card_total)
-puts "Sorry, you hit #{card_total}. Thanks for playing!"
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-card1 = deal_card 
-card2 = deal_card 
-card_total = card1 + card2
-display_card_total(card_total)
-return card_total 
+  card_one = deal_card
+  card_two = deal_card
+  card_total = card_one + card_two
+  display_card_total(card_total) 
+  return card_total
 end
 
 def hit?(card_total)
@@ -43,9 +43,9 @@ def hit?(card_total)
 end
 
 def invalid_command
-puts "Please enter a valid command"
-prompt_user
-get_user_input
+  puts "Please enter a valid command"
+  prompt_user
+  get_user_input
 end
 
 #####################################################
@@ -54,13 +54,12 @@ end
 
 def runner
   welcome 
-
-  card_total = initial_round
-  until card_total > 21
+  card_total = initial_round 
+  until card_total > 21 
     card_total = hit?(card_total)
     display_card_total(card_total)
   end
   end_game(card_total) 
 end
-    
-    runner 
+
+runner 
